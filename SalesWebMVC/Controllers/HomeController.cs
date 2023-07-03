7e638668f1d411d8a7c2a776d883e2d9fa1a1406
Matheus Correa <mattheuuz@outlook.com>
2023-07-03 13:48:41 -0300

    First controller and razor page tests(Added page About to nav-bar)

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -18,6 +18,10 @@ namespace SalesWebMVC.Controllers {
             return View();
         }
 
+        public IActionResult About() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
