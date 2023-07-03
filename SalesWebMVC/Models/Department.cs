d0d455feba5c4515601d0955a1fe2a4217c33505
Matheus Correa <mattheuuz@outlook.com>
2023-07-03 14:34:03 -0300

    Created Departments mvc controller

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesWebMVC.Models {
+    public class Department {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
--- SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -1,5 +1,7 @@
-namespace SalesWebMVC.Models {
-    public class ErrorViewModel {
+namespace SalesWebMVC.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
