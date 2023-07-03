cc4a067a089a2a1f5af2d4a0671cd020016d0719
Matheus Correa <mattheuuz@outlook.com>
2023-07-03 16:00:27 -0300

    MySQL adaption and first migration

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using SalesWebMVC.Data;
+
 namespace SalesWebMVC {
     public class Program {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<SalesWebMVCContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.")));
+                options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMVCContext"), 
+                                    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMVCContext")), 
+                                    builder => builder.MigrationsAssembly("SalesWebMVC")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
