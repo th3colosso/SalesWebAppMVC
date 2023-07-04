a0a2792c965c9332b361096940a9ed675d854258
Matheus Correa <mattheuuz@outlook.com>
2023-07-04 00:31:06 -0300

    validations

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -4,14 +4,21 @@ using System.ComponentModel.DataAnnotations;
 namespace SalesWebMVC.Models {
     public class Seller {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required!")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size must be between {2} and {1}!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "{0} required!")]
+        [EmailAddress(ErrorMessage = "Enter a valid email!")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "{0} required!")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "{0} required!")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}!")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
