ff954342fb27caa54353b3ba25b339ee9989758a
Matheus Correa <mattheuuz@outlook.com>
2023-07-03 22:00:03 -0300

    FK not null (referential integrity on entity Seller)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -14,6 +14,7 @@ namespace SalesWebMVC.Services {
         }
 
         public void Insert(Seller obj) {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
